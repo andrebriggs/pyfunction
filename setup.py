d373705e40f7b87297e3a429858ef44c863d2dcb
Andre <me@andrebriggs.com>
2018-10-10 00:08:09 -0700

    Trailing whitespace

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='pyfunction',
     version='0.1dev',
     packages=['HttpTriggerPython'],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license', 
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
 )
