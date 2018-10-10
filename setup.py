539023e814cc7ce8a238339aa51a8887b268be40
Andre <me@andrebriggs.com>
2018-10-09 23:49:14 -0700

    Added a setup.py and modified the yaml to run lint

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='pyfunction',
+    version='0.1dev',
+    packages=['HttpTriggerPython',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
