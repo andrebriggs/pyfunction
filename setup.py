372a85a1e83877fd7cf968762831f4a8fc0bd3b1
Andre <me@andrebriggs.com>
2018-10-10 00:06:40 -0700

    Issues

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='pyfunction',
     version='0.1dev',
-    packages=['HttpTriggerPython',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    packages=['HttpTriggerPython'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license', 
 )
