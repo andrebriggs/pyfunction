331ede947929b70063ec02520505929d469d85f3
Andre Briggs <andrebriggs@Spartan-Macbook.local>
2018-10-11 14:38:54 -0700

    Returning json

--- Tagger/__init__.py
+++ Tagger/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 import azure.functions as func
 
@@ -14,9 +15,14 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             imageCount = req_body.get('imageCount')
+    
+    data = {}
+    data['key'] = 'value'
+    json_data = json.dumps(data)
 
     if imageCount:
-        return func.HttpResponse(f"You requested {imageCount} images!")
+        return func.HttpResponse(json_data)
+        #return func.HttpResponse(f"You requested {imageCount} images!")
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
