11117cabc48145bcf368a244019bb8997e913810
Andre Briggs <andrebriggs@Spartan-Macbook.local>
2018-10-11 14:33:08 -0700

    Added Tagger

--- Tagger/__init__.py
+++ Tagger/__init__.py
@@ -6,17 +6,17 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    imageCount = req.params.get('imageCount')
+    if not imageCount:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            imageCount = req_body.get('imageCount')
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
+    if imageCount:
+        return func.HttpResponse(f"You requested {imageCount} images!")
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
