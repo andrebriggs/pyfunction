53fd7d0d0e4f3c97a15b586e6aba28fb8923f271
Andre Briggs <andrebriggs@Spartan-Macbook.local>
2018-10-11 14:18:59 -0700

    Added another function

---
+++ Weather/__init__.py
@@ -0,0 +1,9 @@
+import logging
+import datetime
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse(f"The weather is sunny at {datetime.datetime.now()}!")
