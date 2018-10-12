061ba31901ae11c9e20f9b77e7e1a527b0b6f163
Andre Briggs <andrebriggs@Spartan-Macbook.local>
2018-10-11 20:39:06 -0700

    Added stub

---
+++ tests/test_utilities.py
@@ -0,0 +1,15 @@
+'''
+import unittest
+from ... utilities import (getUrls)
+
+class TestUtilityOperations(unittest.TestCase):
+    def test_urls(self):
+        actualArray = getUrls()
+        self.assertEqual(DEFAULT_NUM_IMAGES, downloaded_image_count)
+        with self.assertRaises(NotImplementedError):
+            
+
+
+if __name__ == '__main__':
+    unittest.main()
+    '''
