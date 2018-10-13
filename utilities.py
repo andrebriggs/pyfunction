f526a4fb1f0a490bdd8e22a4a9fa5094a5a8ed8f
Andre <me@andrebriggs.com>
2018-10-13 14:58:48 -0700

    Refactored

--- utilities.py
+++ utilities.py
@@ -1,15 +1,23 @@
-import json
+import sys
 
-def getUrls():
+def getMockDataProvider(numberOfElements):
+        urls =  [
+            'https://csehackstorage.blob.core.windows.net/image-to-tag/image1.jpeg',
+            'https://csehackstorage.blob.core.windows.net/image-to-tag/image2.jpeg',
+            'https://csehackstorage.blob.core.windows.net/image-to-tag/image3.jpeg',
+            'https://csehackstorage.blob.core.windows.net/image-to-tag/image4.jpeg',
+            'https://csehackstorage.blob.core.windows.net/image-to-tag/image5.jpeg'
+        ]
+        return urls[:numberOfElements]
+
+def getImageDictionary(count):
     data = {}    
-    urls =  ['https://csehackstorage.blob.core.windows.net/image-to-tag/image1.jpeg']
-    data['urls'] = urls
-    data['count'] = 1
-    json_data = json.dumps(data)
-    return json_data
+    data['urls'] = getMockDataProvider(count)
+    data['count'] = len(data['urls'])
+    return data
 
-def main():
-    print(getUrls())
+def main(count):
+    print(getImageDictionary(count))
 
 if __name__ == "__main__":
-    main()
+    main(int(sys.argv[1]))
